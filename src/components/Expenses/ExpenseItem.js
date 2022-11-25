e9823d0482325939cf5946114183d19787884fa6
rahmou1 <ahmedrahmou11694@gmail.com>
2022-11-25 23:20:53 +0200

    adding useState and form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,22 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import React, { useState } from "react";
+
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    setTitle("Hello");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount} EGP</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
