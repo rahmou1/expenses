17eda8c4dbdcfc1819d206d2b2c4a003b5dc6f12
rahmou1 <ahmedrahmou11694@gmail.com>
2022-11-26 18:55:04 +0200

    adding button to show and close the adding expenses

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 const NewExpense = (props) => {
+  const [isEditing, setIsEdititng] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEdititng(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEdititng(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEdititng(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
